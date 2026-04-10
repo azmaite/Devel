83d21cb529a3763e82881348f081b8c34ae2346e
Maite Azcorra <azmaite@gmail.com>
2026-04-10 15:00:50 +0200

    finished working and useful widget with dots and outline of all dots - will edit to plot outlines of segments

--- Pupa_muscle_imaging/interactive_alignment.py
+++ Pupa_muscle_imaging/interactive_alignment.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 import matplotlib.colors as mcolors
+from scipy.spatial import Delaunay
+import shapely.geometry as geom
+import shapely.ops as ops
 
 
 def _rotate_points(points, rx, ry, rz):
@@ -60,17 +63,59 @@ def _get_points_and_colors(coordinates_dict):
     points_3d = []
     points_color = []
     for seg in plot_order:
-        points_3d.append(coordinates_dict[seg])
+        segment_points = coordinates_dict[seg]
+
+        # subsample to half the points randomly to reduce clutter (except COXA which has fewer points)
+        if 'COXA' not in seg:
+            indices = np.random.choice(segment_points.shape[0], size=segment_points.shape[0]//2, replace=False)
+            segment_points = segment_points[indices]
+
+        points_3d.append(segment_points)
         segment_name_short = f'{seg.split()[1]} {seg.split()[3]}'   # e.g. "Hind COXA" from "Left Hind leg COXA"
         # assign same color to all points in segment
         color = colors_dict[segment_name_short]
-        points_color.append(np.tile(color, (coordinates_dict[seg].shape[0], 1)))  
+        points_color.append(np.tile(color, (segment_points.shape[0], 1)))  
 
     # concatenate all points and colors into single arrays
     points_3d = np.concatenate(points_3d, axis=0)
     points_color = np.concatenate(points_color, axis=0)
     
     return points_3d, points_color
+
+
+def _alpha_shape(points, alpha=0.04):
+    """ Obtain the alpha shape (concave hull) of a set of points."""
+    tri = Delaunay(points)
+    edges = set()
+
+    def add_edge(i, j):
+        if (i, j) in edges or (j, i) in edges:
+            edges.discard((j, i))
+        else:
+            edges.add((i, j))
+
+    for ia, ib, ic in tri.simplices:
+        pa, pb, pc = points[[ia, ib, ic]]
+        a = np.linalg.norm(pb - pc)
+        b = np.linalg.norm(pa - pc)
+        c = np.linalg.norm(pa - pb)
+        s = (a + b + c) / 2.0
+        area = np.sqrt(max(s*(s-a)*(s-b)*(s-c), 0))
+        if area == 0:
+            continue
+        circum_r = a*b*c / (4.0*area)
+        if circum_r < 1.0 / alpha:
+            add_edge(ia, ib)
+            add_edge(ib, ic)
+            add_edge(ic, ia)
+
+    edge_points = [(points[i], points[j]) for i, j in edges]
+    m = geom.MultiLineString(edge_points)
+
+    polys = list(ops.polygonize(m))
+    outline = np.array(polys[0].exterior.coords)  # get coordinates of the first polygon's exterior as the outline
+
+    return outline
 
 
 
@@ -83,40 +128,69 @@ def interactive_alignment(image, points_3d):
     else:
         colors = None
 
-    # Subsample points if the cloud is too dense for smooth interaction
-    if len(points_3d) > 5000:
-        indices = np.random.choice(len(points_3d), 5000, replace=False)
-        points_3d = points_3d[indices]
-        if colors is not None:
-            colors = colors[indices]
 
-
-    fig, ax = plt.subplots(figsize=(10, 9))
-    plt.subplots_adjust(bottom=0.35)
+    fig, ax = plt.subplots(figsize=(18, 8))
+    plt.subplots_adjust(right=0.75, left=0)
     
     # Initial Plot
     ax.imshow(image, cmap='gray')
     scatter = ax.scatter([], [], s=1, alpha=0.6, c=[] if colors is not None else 'red')
     ax.set_title("Align 3D Object (Red) to Image\nClose window or click 'Done' to finish")
 
+
+    # create a persistant reference for the outline plot
+    outline_plot, = ax.plot([], [], 'k-', lw=2, zorder=5)
+
+    # create a timer
+    timer = fig.canvas.new_timer(interval=200)  
+
+    def compute_and_draw_hull():
+        """ only runs when the timer finishes """
+
+        # get current transformation values from sliders
+        s = s_scale.val
+        rx, ry, rz = s_rx.val, s_ry.val, s_rz.val
+        tx, ty = s_tx.val, s_ty.val
+
+        # Transform points
+        p_transformed = _rotate_points(points_3d * s, rx, ry, rz)
+        x_proj = p_transformed[:, 0] + tx
+        y_proj = p_transformed[:, 1] + ty
+        points_2d = np.column_stack((x_proj, y_proj))
+
+        # Compute alpha shape and plot
+        outline = _alpha_shape(points_2d, alpha=0.04)
+        outline_plot.set_data(outline[:, 0], outline[:, 1])  
+
+        fig.canvas.draw_idle()  # redraw the figure to show the updated hull
+
+    # link the timer to the hull function
+    timer.add_callback(compute_and_draw_hull)
+        
+
     # Slider Axes
-    ax_color = 'lightgoldenrodyellow'
-    ax_scale = plt.axes([0.15, 0.25, 0.7, 0.025], facecolor=ax_color)
-    ax_rx    = plt.axes([0.15, 0.21, 0.7, 0.025], facecolor=ax_color)
-    ax_ry    = plt.axes([0.15, 0.17, 0.7, 0.025], facecolor=ax_color)
-    ax_rz    = plt.axes([0.15, 0.13, 0.7, 0.025], facecolor=ax_color)
-    ax_tx    = plt.axes([0.15, 0.09, 0.7, 0.025], facecolor=ax_color)
-    ax_ty    = plt.axes([0.15, 0.05, 0.7, 0.025], facecolor=ax_color)
+    left, width, height = 0.74, 0.21, 0.025 # right is different for each
+    ax_scale = plt.axes([left, 0.80, width, height])
+    ax_rx    = plt.axes([left, 0.70, width, height])
+    ax_ry    = plt.axes([left, 0.65, width, height])
+    ax_rz    = plt.axes([left, 0.60, width, height])
+    ax_tx    = plt.axes([left, 0.50, width, height])
+    ax_ty    = plt.axes([left, 0.45, width, height])
 
     # Sliders
-    s_scale = Slider(ax_scale, 'Scale', 0.1, 5, valinit=0.6)
-    s_rx = Slider(ax_rx, 'Rot X', -180, 180, valinit=90)
-    s_ry = Slider(ax_ry, 'Rot Y', -180, 180, valinit=0)
-    s_rz = Slider(ax_rz, 'Rot Z', -180, 180, valinit=-90)
-    s_tx = Slider(ax_tx, 'Shift X', 0, image.shape[1], valinit=image.shape[1]//2)
-    s_ty = Slider(ax_ty, 'Shift Y', 0, image.shape[0], valinit=image.shape[0]//2)
+    s_scale = Slider(ax_scale, 'Scale', 0.3, 1.5, valinit=0.6, valstep=0.01, valfmt='%.2f')
+    s_rx = Slider(ax_rx, 'Rot X', -90, 270, valinit=90, valstep=1, valfmt='%d°')
+    s_ry = Slider(ax_ry, 'Rot Y', -180, 180, valinit=0, valstep=1, valfmt='%d°')
+    s_rz = Slider(ax_rz, 'Rot Z', -270, 90, valinit=-90, valstep=1, valfmt='%d°')
+    s_tx = Slider(ax_tx, 'Shift X', 0, image.shape[1], valinit=image.shape[1]//2, valstep=1, valfmt='%d')
+    s_ty = Slider(ax_ty, 'Shift Y', 0, image.shape[0], valinit=image.shape[0]//2, valstep=1, valfmt='%d')
 
     def update(val):
+        """ this runs continuoulsy as sliders are moved, but we will only compute the hull after a short delay to avoid lag """
+
+        # stop the timer every time the sliders are moved
+        timer.stop()
+
         # Transform
         p_transformed = _rotate_points(points_3d * s_scale.val, s_rx.val, s_ry.val, s_rz.val)
         
@@ -130,6 +204,9 @@ def interactive_alignment(image, points_3d):
         if colors is not None:
             scatter.set_color(colors)
 
+        # restart the stimer. If 5000ms pass without the sliders being moved again, the hull will be computed and drawn
+        timer.start()
+
         fig.canvas.draw_idle()
 
     # Register update function
@@ -137,8 +214,10 @@ def interactive_alignment(image, points_3d):
         s.on_changed(update)
 
     # Reset Button
-    reset_ax = plt.axes([0.65, 0.29, 0.1, 0.04])
-    btn_reset = Button(reset_ax, 'Reset', color=ax_color, hovercolor='0.975')
+    button_height = 0.35
+    first_button_x = 0.775
+    reset_ax = plt.axes([first_button_x, button_height, 0.07, 0.04])
+    btn_reset = Button(reset_ax, 'Reset', color='lightgray', hovercolor='0.975')
 
     def reset(event):
         for s in [s_scale, s_rx, s_ry, s_rz, s_tx, s_ty]:
@@ -146,7 +225,7 @@ def interactive_alignment(image, points_3d):
     btn_reset.on_clicked(reset)
 
     # --- THE DONE BUTTON ---
-    done_ax = plt.axes([0.77, 0.29, 0.1, 0.04])
+    done_ax = plt.axes([first_button_x + 0.08, button_height, 0.07, 0.04])
     btn_done = Button(done_ax, 'Done', color='#90EE90', hovercolor='#32CD32')
 
     def done(event):
@@ -160,6 +239,6 @@ def interactive_alignment(image, points_3d):
     # Return the values captured at the moment the window was closed
     return {
         "scale": s_scale.val,
-        "rotation_deg": (s_rx.val, s_ry.val, s_rz.val),
-        "shift_px": (s_tx.val, s_ty.val)
+        "rotation_deg_xyz": (s_rx.val, s_ry.val, s_rz.val),
+        "shift_px_xy": (s_tx.val, s_ty.val)
     }
--- Pupa_muscle_imaging/test_interactive_alignment.py
+++ Pupa_muscle_imaging/test_interactive_alignment.py
@@ -10,11 +10,11 @@ traces_folder = os.path.join(main_path, 'CT_PupaP15_traced')
 traces_file = os.path.join(traces_folder, 'pupa_traced_coordinates_rot.pkl')
 with open(traces_file, 'rb') as f:
     coordinates = pickle.load(f)
-coordinates_0 = coordinates['Left Front leg TIBIA']
 
 # load min_projection image
-mkv_list = utils.get_mkv_list()
-mkv_file = mkv_list[30]
+#mkv_list = utils.get_mkv_list()
+#mkv_file = mkv_list[30]
+mkv_file = '/mnt/labserver/data/MA/Development_project/Pupa_muscle_long_recordings/20260220_V7_prepup_02-18_11h45/20260221_213031/20260220_152804_V7_prepup_2-18_11h45_fly_1_V7_prepup_2-18_11h45_20260221_213031_raw_tiff.mkv'
 min_proj, _ = utils.load_min_max_proj(mkv_file)
 
 # align
