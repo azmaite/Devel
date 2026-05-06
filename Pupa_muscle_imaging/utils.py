288fe88a6dd3e430e527e03038fb33f1257169b9
Maite Azcorra <azmaite@gmail.com>
2026-05-06 15:00:39 +0200

    Added scripts to merge and intersect masks. Changed default nmf init method to nndsvda. Added input controls

--- Pupa_muscle_imaging/utils.py
+++ Pupa_muscle_imaging/utils.py
@@ -7,6 +7,8 @@ Utility functions for NMF muscle roi detection. Includes functions for:
     unmasked_image = unmask_image(values, mask, bounding_box)
     masked_video = mask_video(video, mask)
     unmasked_video = unmask_video(masked_frames, mask, bounding_box)
+    merged_mask = merge_masks(mask, submask)
+    intersection_mask = intersect_masks(mask1, mask2)
 
 - running NMF and selecting/processing frames for NMF
     frames_flat_dif_norm = diff_norm_frames(frames, background_img=None)
@@ -96,6 +98,33 @@ def unmask_video(masked_frames: np.ndarray,
     """Reconstruct video from masked frames, using unmask_image for each frame."""
     return np.array([unmask_image(frame, mask, bounding_box) for frame in masked_frames])
 
+def merge_masks(mask: np.ndarray,
+                submask: np.ndarray) -> np.ndarray:
+    """
+    Combine a submask with the original mask, returning a new mask with the shape
+    of the original but with the submask selected pixels only.
+    """
+    # check that the submask is a 1d array 
+    # with the same number of elements as positive values in the original mask
+    assert submask.ndim == 1, f'Submask must be a 1D array, but has shape {submask.shape}'
+    assert len(mask) == np.sum(mask), f'Number of pixels in mask {len(mask)} does not match number of pixels in submask {np.sum(submask)}'
+
+    # merge masks
+    new_mask = np.zeros_like(mask)
+    new_mask[submask.astype(bool)] = 1
+
+    return new_mask
+
+def intersect_masks(mask1: np.ndarray,
+                    mask2: np.ndarray) -> np.ndarray:
+    """ Combine two masks by taking the intersection of their positive pixels."""
+    # check that the masks have the same shape
+    assert mask1.shape == mask2.shape, f'Mask shapes {mask1.shape} and {mask2.shape} do not match'
+
+    new_mask = np.zeros_like(mask1)
+    new_mask[(mask1.astype(bool)) & (mask2.astype(bool))] = 1
+    return new_mask
+
 
 
 ### RUNNING NMF FUNCTIONS ###
@@ -122,15 +151,14 @@ def diff_norm_frames(frames: np.ndarray,
 
 
 def nmf_get_temporal(frames: np.ndarray,
-                     H: np.ndarray,
-                     **nmf_kwargs) -> np.ndarray:
+                     H: np.ndarray) -> np.ndarray:
     """Given spatial components H, solve for temporal components W using frames."""
 
-    kwargs: dict[str, Any] = dict(n_components=H.shape[0], max_iter=4000, init='custom')
-    kwargs.update(nmf_kwargs)
-    kwargs['init'] = 'custom'  # always use custom init for this step regardless of user input
+    # check that frames are non-negative and go from 0 to 1
+    assert np.all(frames >= 0), "Frames must be non-negative"
+    assert np.all(frames <= 1), "Frames must be normalized to [0, 1]"
 
-    model = NMF(**kwargs)
+    model = NMF(n_components=H.shape[0], init='custom')
     frames_type = type(frames[0, 0])
     model.components_ = H.astype(frames_type)
     W = model.transform(frames)
@@ -166,10 +194,13 @@ def fit_nmf(frames: np.ndarray,
         W_orig (temporal components for selected frames only).
     """
 
+    # check that frames are non-negative
+    assert np.all(frames >= 0), "Frames must be non-negative"
+
     if selected_idx is None:
         selected_idx = np.arange(frames.shape[0])
 
-    kwargs: dict[str, Any] = dict(n_components=n_components, max_iter=4000, init='random', random_state=0)
+    kwargs: dict[str, Any] = dict(n_components=n_components, max_iter=4000, init='nndsvda')
     kwargs.update(nmf_kwargs)
     model = NMF(**kwargs)
     W_orig = model.fit_transform(frames[selected_idx])
@@ -299,7 +330,8 @@ def plot_spatial_components(H: np.ndarray,
                              bounding_box: np.ndarray,
                              roi: np.ndarray | None = None,
                              order: np.ndarray | list | None = None,
-                             cols: int = 6) -> None:
+                             cols: int = 6,
+                             aspect_ratio : float = 1.0) -> None:
     """Plot the spatial components as images, with the roi contour overlaid (if present).
     If the spatial component (and mask) are 3d, they will be averaged across the z dimension before plotting."""
 
@@ -323,7 +355,7 @@ def plot_spatial_components(H: np.ndarray,
         if img.ndim == 3:
             img = img.mean(axis=0)
 
-        axes[n].imshow(img, cmap='viridis')
+        axes[n].imshow(img, cmap='viridis', aspect=aspect_ratio)
         if roi is not None:
             roi_cropped = roi - bounding_box[:2]
             axes[n].plot(np.array(roi_cropped)[:, 0], np.array(roi_cropped)[:, 1], 'r--', linewidth=0.4)
